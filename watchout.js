24a50475cd16a6353d2e7473b3bd049d0b771a17
neillobo <neillobo@outlook.com>
2014-06-13 16:00:42 -0700

    Set up board with svg and d3

--- watchout.js
+++ watchout.js
@@ -1,2 +1,26 @@
 // start slingin' some d3 here.
 
+//Game Options
+var height = 540;
+var width = 720;
+var numEnemies = 0;
+
+
+//Game Stats
+var currentScore = 0;
+var highScore = 0;
+
+//Establish Player
+//Give color
+//Have him respond to mouse drag and then cursor keys
+
+var svg= d3.select(".game").append("svg")
+  .attr("width", 640)
+  .attr("height", 500);
+
+  svg.append("rect")
+  .attr("x", 0).attr("y", 0 )
+  .attr("width", 640)
+  .attr("height", 500)
+  .style("fill", "silver");
+

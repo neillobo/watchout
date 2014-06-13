e1f426689f5fbe891c21852fb40edec2c72f3147
neillobo <neillobo@outlook.com>
2014-06-13 16:34:38 -0700

    Added enemies as an svg element with player

--- watchout.js
+++ watchout.js
@@ -1,8 +1,10 @@
 // start slingin' some d3 here.
 
 //Game Options
-var height = 540;
-var width = 720;
+var gameOptions = {
+height : 540,
+width : 720
+};
 var numEnemies = 0;
 
 
@@ -15,12 +17,61 @@ var highScore = 0;
 //Have him respond to mouse drag and then cursor keys
 
 var svg= d3.select(".game").append("svg")
-  .attr("width", 640)
-  .attr("height", 500);
+  .attr("width", gameOptions.width)
+  .attr("height", gameOptions.height);
 
   svg.append("rect")
-  .attr("x", 0).attr("y", 0 )
-  .attr("width", 640)
-  .attr("height", 500)
+  .attr({
+    x: 0,
+    y: 0,
+    width: gameOptions.width,
+    height: gameOptions.height,
+  })
   .style("fill", "silver");
+
+
+
+var Player = function(){
+  this.icon =svg.append("circle")
+  .attr({
+    cx: gameOptions.width/2 - 25,
+    cy: gameOptions.height/2 - 25,
+    r: 20
+  }).style("fill", "black");
+
+};
+
+var player = new Player();
+// player.icon.transition().duration(5000).attr("cx", 200)
+//   .transition().duration(5000).attr("cy", 50)
+//   .transition().duration(1000).attr({
+//     cy: gameOptions.height - 50,
+//     cx: gameOptions.width - 50
+//   });
+//
+
+var Enemy = function(x,y){
+  this.icon =svg.append('circle')
+  .attr({
+    cx: x,
+    cy: y,
+    r: 10
+  }).style("fill", "blue");
+};
+
+
+
+var createEnemy = function(){
+  for(var i = 0; i < 30; i++){
+    var x = 20 + Math.floor(Math.random()* (gameOptions.width - 20));
+    var y = 20 + Math.floor(Math.random()* (gameOptions.height - 20));
+    new Enemy(x,y);
+  }
+};
+
+createEnemy();
+
+
+
+
 

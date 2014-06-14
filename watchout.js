5dda5965df7f92e7103b23966914bc3f42c101a9
neillobo <neillobo@outlook.com>
2014-06-14 10:22:10 -0700

    Implement working update function

--- watchout.js
+++ watchout.js
@@ -4,40 +4,70 @@ var Game = {
   numEnemies : 10
 };
 
-var svg = d3.select('.gameBoard').append('svg')
+var svg = d3.select('body').append('svg')
   .attr({
     width : Game.width,
     height : Game.height
-  });
+  }).append('g')
+  .attr("transform", "translate(32,"+ (Game.height / 2) + ")");
 
-var canvas = svg.append('rect')
-  .attr({
-    width : Game.width,
-    height : Game.height
-  }).style('fill','silver');
+var alphabet = [12,23,29,36,51,67,72,85];
 
-var createEnemies = function(){
-  var enemies = [];
-  for(var i = 0; i < Game.numEnemies; i++){
-    enemies[i] = {
-      id: i,
-      x: Math.random() * 100,
-      y: Math.random() * 100,
-    };
+
+
+function update(someData) {
+
+  // DATA JOIN
+  // Join new data with old elements, if any.
+  var circle = svg.selectAll("circle")
+      .data(someData, function(d){ return d;});
+
+  // UPDATE
+  // Update old elements as needed.
+  circle.attr("class", "update");
+
+  // ENTER
+  // Create new elements as needed.
+  circle.enter().append("circle")
+      .attr("class", "enter");
+
+  // ENTER + UPDATE
+  // Appending to the enter selection expands the update selection to include
+  // entering elements; so, operations on the update selection after appending to
+  // the enter selection will apply to both entering and updating nodes.
+  // text.text(function(d) { return d; });
+  circle.attr("cx", function(d, i) { return i * 12; })
+      .attr("cy", function(d){return d/2;})
+      .attr("r", '10')
+      .style('fill', function(d){
+        if(d%2){
+          return 'red';
+        } else{
+          return 'blue';
+        }
+      });
+
+  // EXIT
+  // Remove old elements as needed.
+  circle.exit().remove();
+}
+
+// The initial display.
+update(alphabet);
+
+// Grab a random sample of letters from the alphabet, in alphabetical order.
+setInterval(function() {
+  update(shuffle(alphabet)
+      .slice(0, Math.floor(Math.random() * 10))
+      .sort());
+}, 1500);
+
+// Shuffles the input array.
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m], array[m] = array[i], array[i] = t;
   }
-  return enemies;
-};
-
-var render = function(enemyData){
-  var enemyObjects = canvas.selectAll('circle').data(enemyData, function(d){
-    return d.id;
-  });
-  enemyObjects.enter().append('circle')
-  .attr('cx', 50).attr('cy', 50).attr('r', 5);
-  enemyObjects.exit().remove();
-};
-
-var enemyData = createEnemies();
-
-// render(enemyData);
-
+  return array;
+}
